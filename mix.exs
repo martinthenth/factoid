cf23ce649a4cdeece861ad26851a0a97017919c7
Martin Nijboer <mrnijboer@gmail.com>
2023-06-17 22:40:16 +0200

    Export unique_integer/0 and unique_uuid/0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Factoid.MixProject do
   use Mix.Project
 
-  @version "0.1.1"
+  @version "0.1.2"
   @source_url "https://github.com/recruiterbay/factoid"
   @changelog_url "https://github.com/recruiterbay/factoid/blob/main/CHANGELOG.md"
 
