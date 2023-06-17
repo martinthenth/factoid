1fffe4a1d3057e486fbf45c1e02d1e2ba93b9792
Martin Nijboer <mrnijboer@gmail.com>
2023-06-17 14:09:04 +0200

    Bump version to 0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+# 0.1.1
+
+- Updated documentation
+- Refactors the macro for better documentation
+
 # 0.1.0
 
 - Initial release
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Factoid.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
   @source_url "https://github.com/recruiterbay/factoid"
   @changelog_url "https://github.com/recruiterbay/factoid/blob/main/CHANGELOG.md"
 
